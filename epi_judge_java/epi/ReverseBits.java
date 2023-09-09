5717270611b2c05ea6af887c9ba6fddfa62bfe40
David Janda <djanda97@gmail.com>
2023-09-09 00:36:09 -0700

    Progress on problem 4.3 - ReverseBits

--- epi_judge_java/epi/ReverseBits.java
+++ epi_judge_java/epi/ReverseBits.java
@@ -1,18 +1,33 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class ReverseBits {
   @EpiTest(testDataFile = "reverse_bits.tsv")
   public static long reverseBits(long x) {
-    // TODO - you fill in here.
-    return 0;
+    char[] binary = Long.toBinaryString(x).toCharArray();
+    int start = 0;
+    int end = binary.length - 1;
+    while (start < end) {
+      char tmp = binary[start];
+      binary[start] = binary[end];
+      binary[end] = tmp;
+      start++;
+      end--;
+    }
+    if (String.valueOf(binary).contentEquals(new StringBuilder(Long.toBinaryString(x)).reverse())) {
+      System.out.println("PASSED");
+      System.out.println("Input: " + Long.toBinaryString(x));
+      System.out.println("Output: " + String.valueOf(binary));
+    }
+    return Long.parseLong(String.valueOf(binary));
   }
 
   public static void main(String[] args) {
     System.exit(
-        GenericTest
-            .runFromAnnotations(args, "ReverseBits.java",
-                                new Object() {}.getClass().getEnclosingClass())
+        GenericTest.runFromAnnotations(
+                args, "ReverseBits.java", new Object() {}.getClass().getEnclosingClass())
             .ordinal());
   }
 }
