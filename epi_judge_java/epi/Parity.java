89be6ebac95272c0da8ceff6c80285ea091c9aef
David Janda <djanda97@gmail.com>
2023-09-08 20:01:09 -0700

    Solve problem 4.1 - Parity

--- epi_judge_java/epi/Parity.java
+++ epi_judge_java/epi/Parity.java
@@ -1,18 +1,28 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class Parity {
   @EpiTest(testDataFile = "parity.tsv")
   public static short parity(long x) {
-    // TODO - you fill in here.
-    return 0;
+    short count = 0;
+    for (char c : Long.toBinaryString(x).toCharArray()) {
+      if (c == '1') {
+        count++;
+      }
+    }
+
+    short result = 0;
+    if (count % 2 != 0) {
+      result = 1;
+    }
+
+    return result;
   }
 
   public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "Parity.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
+    System.exit(GenericTest.runFromAnnotations(args, "Parity.java", new Object() {
+    }.getClass().getEnclosingClass()).ordinal());
   }
 }
