ad0193a4eb612f479bd60f74710703f4d3452908
David Janda <djanda97@gmail.com>
2023-09-09 20:32:23 -0700

    Solve problem 4.9 - IsNumberPalindromic

--- epi_judge_java/epi/IsNumberPalindromic.java
+++ epi_judge_java/epi/IsNumberPalindromic.java
@@ -1,18 +1,28 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class IsNumberPalindromic {
   @EpiTest(testDataFile = "is_number_palindromic.tsv")
   public static boolean isPalindromeNumber(int x) {
-    // TODO - you fill in here.
+    char[] c = String.valueOf(x).toCharArray();
+    int start = 0;
+    int end = c.length - 1;
+    while (start < end) {
+      if (c[start] != c[end]) {
+        return false;
+      }
+      start++;
+      end--;
+    }
     return true;
   }
 
   public static void main(String[] args) {
     System.exit(
-        GenericTest
-            .runFromAnnotations(args, "IsNumberPalindromic.java",
-                                new Object() {}.getClass().getEnclosingClass())
+        GenericTest.runFromAnnotations(
+                args, "IsNumberPalindromic.java", new Object() {}.getClass().getEnclosingClass())
             .ordinal());
   }
 }
