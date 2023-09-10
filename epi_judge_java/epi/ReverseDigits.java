9844952ea81cc75138844cec99a856c54945a160
David Janda <djanda97@gmail.com>
2023-09-09 20:31:58 -0700

    Solve problem 4.8 - ReverseDigits

--- epi_judge_java/epi/ReverseDigits.java
+++ epi_judge_java/epi/ReverseDigits.java
@@ -1,18 +1,26 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class ReverseDigits {
   @EpiTest(testDataFile = "reverse_digits.tsv")
   public static long reverse(int x) {
-    // TODO - you fill in here.
-    return 0;
+    String s = String.valueOf(x);
+    StringBuilder sb = new StringBuilder();
+    boolean isNegative = s.charAt(0) == '-';
+    if (isNegative) {
+      sb.append(s.substring(1).toCharArray());
+      return Long.parseLong("-" + sb.reverse());
+    }
+    sb.append(s.toCharArray());
+    return Long.parseLong(sb.reverse().toString());
   }
 
   public static void main(String[] args) {
     System.exit(
-        GenericTest
-            .runFromAnnotations(args, "ReverseDigits.java",
-                                new Object() {}.getClass().getEnclosingClass())
+        GenericTest.runFromAnnotations(
+                args, "ReverseDigits.java", new Object() {}.getClass().getEnclosingClass())
             .ordinal());
   }
 }
