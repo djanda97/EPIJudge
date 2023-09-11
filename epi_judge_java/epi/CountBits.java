21ce5790c628aded7b2ebaea196b77a8f151faf9
David Janda <djanda97@gmail.com>
2023-09-10 19:47:06 -0700

    Solve problem - CountBits

--- epi_judge_java/epi/CountBits.java
+++ epi_judge_java/epi/CountBits.java
@@ -1,19 +1,25 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class CountBits {
   @EpiTest(testDataFile = "count_bits.tsv")
-
   public static short countBits(int x) {
-    // TODO - you fill in here.
-    return 0;
+    char[] binary = Integer.toBinaryString(x).toCharArray();
+    short count = 0;
+    for (char c : binary) {
+      if (c == '1') {
+        count++;
+      }
+    }
+    return count;
   }
 
   public static void main(String[] args) {
     System.exit(
-        GenericTest
-            .runFromAnnotations(args, "CountBits.java",
-                                new Object() {}.getClass().getEnclosingClass())
+        GenericTest.runFromAnnotations(
+                args, "CountBits.java", new Object() {}.getClass().getEnclosingClass())
             .ordinal());
   }
 }
